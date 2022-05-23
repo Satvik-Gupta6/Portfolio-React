7af6cdfbdea24059dfda30602bcb3b537e7eb4c5
SATVIK GUPTA <satvikg369@gmail.com>
2022-05-23 19:44:34 +0530

    subtitle change

--- src/components/Home/Type.js
+++ src/components/Home/Type.js
@@ -7,13 +7,13 @@ function Type() {
       options={{
         strings: [
           "Developer",
-          "Deep Learning Engineer",
+          "Just getting started 📈",
           "MERN Stack Developer",
           "Open Source Contributor",
         ],
         autoStart: true,
         loop: true,
-        deleteSpeed: 50,
+        deleteSpeed: 40,
       }}
     />
   );
